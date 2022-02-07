60f18a52133b9b00c0e7e80d035aebc464d452b5
Chad <chad__thomas@hotmail.com>
2022-02-07 12:16:19 -0700

    Delete db.js

--- server/db/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://DATABASE_USER:DATABASE_PASSWORD@127.0.0.1:5432/messenger", {
-  logging: false
-});
-
-module.exports = db;
