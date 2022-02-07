deb53754ca680c145213ea4a889da0b7f49d43a6
Chad <chad__thomas@hotmail.com>
2022-02-07 16:45:27 -0700

    Update db.js
    The Tests are still failing on Cypress. I see errors about the connection being refused at 127.0.0.1:3000. I'm thinking that might not be a smart way for me to connect to the database since it is possible to be running on a different local IP. Using Localhost seems like a more universal option.

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://DATABASE_USER:DATABASE_PASSWORD@127.0.0.1:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://DATABASE_USER:DATABASE_PASSWORD@localhost:5432/messenger", {
   logging: false
 });
 
