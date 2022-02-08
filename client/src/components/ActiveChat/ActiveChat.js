b1e42e441a1d644e0175eca76b106229313de175
Chad <chad__thomas@hotmail.com>
2022-02-08 10:45:56 -0700

    update to db.js environment variables
    I changed the Sequelize connection to the postgres database to be a string literal, and correctly use the environment variables from my .env file.
    
    I also removed the useSelector import that wasn't being used in ActiveChat anymore.

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { Input, Header, Messages } from "./index";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 
 const useStyles = makeStyles(() => ({
   root: {
